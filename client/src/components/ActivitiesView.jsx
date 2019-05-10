24959e9849ded2cfb6e0f443d5a1cb82e7eb276c
Ed Roesch <edroesch@gmail.com>
2019-05-10 07:31:44 -0400

    displays activities properly

--- client/src/components/ActivitiesView.jsx
+++ client/src/components/ActivitiesView.jsx
@@ -10,19 +10,36 @@ function ActivitiesView(props) {
 
 <h1>{props.username}'s Logged Activities</h1>
 
+<div className="act-headers">
+Hours &nbsp; Name &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Date  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Category
+</div>
       {props.activities.map(activity => (
         <div
           key={activity.id}
           className="activity-card"
           onClick={(e) => {
             props.history.push(`activities/${activity.id}`);
-            // props.history.push(`users/${props.user_id}/activities/${activity.id}`);
             window.scrollTo(0, 0);
           }}>
-            <p>{activity.category}</p>
-          <h3>
-            <p>{activity.name}</p>
-          </h3>
+            <p className="activity-row">
+            <span className="hours-spent">
+            {activity.hours_spent? activity.hours_spent : "na" } 
+            </span>
+            &nbsp; 
+            <span className="name">
+            {activity.name? activity.name : "na" } 
+            </span>
+            &nbsp; 
+            <span className="date">
+            {activity.date? activity.date : "na" } 
+            </span> 
+            &nbsp; 
+            <span className="category">
+            {activity.category? activity.category : "na" } 
+            </span>
+            </p>
+          
+
         </div>
       ))}
       
